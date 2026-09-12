d0145b2bcbd0aa73fdc22c3085c517fcd2ab806b
Ritesh Kumar <sidharthrk7890@gmail.com>
2026-09-12 11:37:49 +0530

    Data Type

---
+++ 01_BASIC_OF_JS/04_Data_Type.js
@@ -0,0 +1,26 @@
+//Data type 
+"use strict";  // treat all JS code as newer version 
+
+// aleart( 3 + 3) // WE ARE using node js , not browser 
+
+console.log( 3 +3) // code readability should be high 
+console.log("Ritesh")
+
+let name = "Ritesh" // string 
+let age = 18   // number 
+let isLoggedIn = false // boolean 
+//data type 
+//number => 2 to power 53
+// bigint 
+// string = ""
+//boolean => true/false 
+//null => standalone vale
+// undefined => 
+// symbol =>
+
+//Object 
+
+console.log(typeof age ); // number 
+console.log(typeof null); // object 
+console.log(typeof undefined); // undefined 
+
