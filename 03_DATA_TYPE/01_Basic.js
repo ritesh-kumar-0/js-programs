6e2a33dc2a8488b7b8f22e052a538410970cac68
Ritesh Kumar <sidharthrk7890@gmail.com>
2026-09-13 22:15:07 +0530

    Data Type

---
+++ 03_DATA_TYPE/01_Basic.js
@@ -0,0 +1,11 @@
+//A data type defines what kind of value a variable can store and what operations can be performed on that value.
+let age = 20;  // 20 - number
+let name = "Ritesh" // Ritesh - string 
+let isStudent = true; // true - Boolean 
+
+// Classification of JavaScript Data Types
+// 1. Primitive  2. Non- Primitive Types 
+// JavaScript has 7 primitive data types:
+// 1. Number 2. string 3. boolean 4. Undefined 5. Null 6. BigInt 7.Symbol
+
+//JavaScript has 7 primitive data types: Object 
---
+++ 03_DATA_TYPE/02_Number.js
@@ -0,0 +1,12 @@
+//The Number data type is used to represent both integer and floating-point (decimal)
+let age = 20;
+let marks = 85.5;
+let temperature = -10;
+
+console.log(age);
+console.log(marks);
+console.log(temperature);
+
+console.log(typeof marks);
+
+console.log("Hello" * 5) // NaN
